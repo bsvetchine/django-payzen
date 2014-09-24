b8d02b76fd42de6006ff96fc4eb68764df041dd1
Bertrand Svetchine <bsvetchine@tandoori.pro>
2014-09-24 18:49:28 +0200

    Enable admin site for django_payzen.

--- django_payzen/admin.py
+++ django_payzen/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+class PaymentRequestAdmin(admin.ModelAdmin):
+    model = models.PaymentRequest
+    list_display = ("vads_amount", "get_vads_currency_display",
+                    "vads_trans_id", "vads_trans_date")
+
+
+class PaymentResponseAdmin(admin.ModelAdmin):
+    model = models.PaymentResponse
+    list_display = ("vads_amount", "get_vads_currency_display",
+                    "vads_trans_id", "vads_trans_date",
+                    "vads_operation_type")
+
+
+admin.site.register(models.PaymentRequest, PaymentRequestAdmin)
+admin.site.register(models.PaymentResponse, PaymentResponseAdmin)

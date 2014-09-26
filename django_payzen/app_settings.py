8157ee43f31bd106d00c86ac4a7bc35a79c29e41
Bertrand Svetchine <bsvetchine@tandoori.pro>
2014-09-26 16:41:31 +0200

    Set essential payzen settings as mandatory for django_payzen.

--- django_payzen/app_settings.py
+++ django_payzen/app_settings.py
@@ -10,8 +10,8 @@ from django.conf import settings
 
 PAYZEN_REQUEST_URL = 'https://secure.payzen.eu/vads-payment/'
 VADS_CONTRIB = 'django-payzen v0.9'
-VADS_SITE_ID = getattr(settings, 'VADS_SITE_ID', None)
-CLIENT_CERTIFICATE = getattr(settings, 'VADS_CERTIFICATE', None)
+VADS_SITE_ID = getattr(settings, 'VADS_SITE_ID')
+VADS_CERTIFICATE = getattr(settings, 'VADS_CERTIFICATE')
 VADS_CURRENCY = getattr(settings, 'VADS_CURRENCY', None)
 VADS_ACTION_MODE = getattr(settings, 'VADS_ACTION_MODE', None)
 VADS_CTX_MODE = getattr(settings, 'VADS_CTX_MODE', None)

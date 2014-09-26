24be2777b23fe3d9d545297ba8502fa87c8d5d60
Bertrand Svetchine <bsvetchine@tandoori.pro>
2014-09-26 16:43:01 +0200

    Add a auto submit form functionnality.

--- django_payzen/templatetags/payzen_extras.py
+++ django_payzen/templatetags/payzen_extras.py
@@ -7,10 +7,14 @@ register = template.base.Library()
 
 
 @register.simple_tag
-def payzen_form(payment_request):
+def payzen_form(payment_request, auto_submit=False):
     """TODO docstring."""
+    if auto_submit:
+        template_used = "django_payzen/auto_submit_form.html"
+    else:
+        template_used = "django_payzen/form.html"
     payment_request.update()
-    t = template.loader.get_template("django_payzen/form.html")
+    t = template.loader.get_template(template_used)
     return t.render(template.Context({
         "form": forms.PaymentRequestForm(instance=payment_request),
         "payzen_submit_url": app_settings.PAYZEN_REQUEST_URL

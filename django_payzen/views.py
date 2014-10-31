f7e9e05b0d4f0c80636502d5d2e634c64ab184de
Bertrand Svetchine <bsvetchine@tandoori.pro>
2014-10-31 11:16:35 +0100

    Fixed bug in PaymentResponse update call.

--- django_payzen/views.py
+++ django_payzen/views.py
@@ -24,29 +24,29 @@ class ResponseView(generic.View):
                 "Django-Payzen : Response signature detected as invalid",
                 extra={"stack": True})
             return http.HttpResponse()
-        # Payzen data is checked and valid
-        form = forms.PaymentResponseForm(request.POST)
+        # The signature is valid
+        vads_trans_id = request.POST.get("vads_trans_id")
+        vads_trans_date = request.POST.get("vads_trans_date")
+        vads_site_id = request.POST.get("vads_site_id")
+        try:
+            instance = models.PaymentResponse.objects.get(
+                vads_trans_id=vads_trans_id,
+                vads_trans_date=vads_trans_date,
+                vads_site_id=vads_site_id)
+            form = forms.PaymentResponseForm(request.POST, instance=instance)
+        except models.PaymentResponse.DoesNotExist:
+            form = forms.PaymentResponseForm(request.POST)
         if form.is_valid():
-            try:
-                vads_trans_id = form.cleaned_data.get("vads_trans_id")
-                vads_trans_date = form.cleaned_data.get("vads_trans_date")
-                vads_site_id = form.cleaned_data.get("vads_site_id")
-                instance = models.PaymentResponse.objects.get(
-                    vads_trans_id=vads_trans_id,
-                    vads_trans_date=vads_trans_date,
-                    vads_site_id=vads_site_id)
-                response = form.save(instance=instance)
-            except models.PaymentResponse.DoesNotExist:
-                response = form.save()
-            logger.info("Django-Payzen : Transaction {} response received !"
-                        .format(response.vads_trans_id))
+            response = form.save()
             if response.payment_successful:
                 signal = signals.payment_success
             else:
                 signal = signals.payment_failure
             signal.send(sender=self.__class__, response=response)
+            logger.info("Django-Payzen : Transaction {} response received !"
+                        .format(response.vads_trans_id))
         else:
+            signals.response_error.send(sender=self.__class__)
             logger.error("Django-Payzen : Response could not be saved - {}"
                          .format(form.errors), extra={"stack": True})
-            signals.response_error.send(sender=self.__class__)
         return http.HttpResponse()

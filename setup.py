300e52859cf6ea30e2cd3234041047204de170c3
Bertrand Svetchine <bsvetchine@tandoori.pro>
2014-12-21 22:30:39 +0100

    Updated requirements.

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 """Setup file for easy installation"""
 import os
-import re
 
 from setuptools import setup, find_packages
 
@@ -9,25 +8,6 @@ from tests import test_cmd
 
 ROOT = os.path.dirname(__file__)
 PIP_REQUIRES = os.path.join(ROOT, "requirements.txt")
-
-
-def parse_requirements(*filenames):
-    requirements = []
-    for f in filenames:
-        for line in open(f, 'r').read().split('\n'):
-            # Comment lines. Skip.
-            if re.match(r'(\s*#)|(\s*$)', line):
-                continue
-            # Editable matches. Put the egg name into our reqs list.
-            if re.match(r'\s*-e\s+', line):
-                pkg = re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line)
-                requirements.append("%s" % pkg)
-            # File-based installs not supported/needed. Skip.
-            elif re.match(r'\s*-f\s+', line):
-                pass
-            else:
-                requirements.append(line)
-    return requirements
 
 
 setup(
@@ -40,7 +20,7 @@ setup(
     url="https://github.com/bsvetchine/django-payzen",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=parse_requirements(PIP_REQUIRES),
+    install_requires=["Django"],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",

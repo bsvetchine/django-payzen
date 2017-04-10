f53bb51b4be3d12943512bb84a6508cec6c540b5
Felipe Gomes Vieira Ferreira <felipe.ferreira@mapptv.com.br>
2017-04-10 16:28:31 -0300

    Code fix to django 1.10

--- django_payzen/urls.py
+++ django_payzen/urls.py
@@ -1,9 +1,8 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import  url
 
 from . import views
 
-urlpatterns = patterns(
-    "",
+urlpatterns = [
     url(r"^response/$", views.ResponseView.as_view(),
         name="django_payzen_response"),
-)
+]
